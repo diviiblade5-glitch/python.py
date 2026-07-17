e892dd24a9a6f95dc2044096efd2f98a686130ac
diviiblade5-glitch <diviiblade5@gmail.com>
2026-07-17 09:36:23 +0100

    learned variables

---
+++ variable.py
@@ -0,0 +1,9 @@
+Name = input("what is your name? ")
+Hero = input("who is your favourite hero? ")
+Time =input("how many times have you watched it? ")
+Colour =input("what is your favourite colour? ")
+
+print(f"Hello {Name}!")
+print(f"your favourite hero is {Hero}.") 
+print(f"you have watched it {Time} times.")
+print(f"your favourite colour is {Colour}.")
