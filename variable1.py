e06c4bc48da57e8ce7575febcf1207168542d28f
diviiblade5-glitch <diviiblade5@gmail.com>
2026-07-17 10:43:47 +0100

    learned variables1

---
+++ variable1.py
@@ -0,0 +1,9 @@
+Name = input("what is your name?: ")
+Age = input("what is your age?: ")
+
+Age = int(Age) #convert age to an integer
+
+Next_year_age = Age + 1 #calculate age next year
+
+print(f"hello {Name}!")
+print(f"next year you will be {Next_year_age} years old.")
