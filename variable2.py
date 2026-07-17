a142f086f49f5b6a3e9ade16183819a1b3a9f672
diviiblade5-glitch <diviiblade5@gmail.com>
2026-07-17 11:29:19 +0100

    learned variables2

---
+++ variable2.py
@@ -0,0 +1,14 @@
+name = input("what is your name?: ")
+age = input("what is your age?: ")
+
+age = int(age) #convert age to an integer
+
+age_in_5 = age + 5
+age_in_10 = age + 10
+
+print(f"Hello {name}!")
+print(f"you are {age} years old")
+print(f"in five years you'll be {age_in_5} years old")
+print(f"in 10 years you will be {age_in_10} years old")
+
+
