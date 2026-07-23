c004f3cfad0326f544eee4c5ca45342cd0a385f3
diviiblade5-glitch <diviiblade5@gmail.com>
2026-07-23 11:53:40 +0100

    fruits

---
+++ fruits.py
@@ -0,0 +1,10 @@
+fruits = ["apple", "banana", "cherry"]
+
+fruits[0]= "eggplant"
+fruits.append("mango")
+fruits.remove("cherry")
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(fruits)
