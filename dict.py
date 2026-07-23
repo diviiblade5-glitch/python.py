d542a4f5a278e7c12e08affe6ffd212a4e66a601
diviiblade5-glitch <diviiblade5@gmail.com>
2026-07-23 14:11:19 +0100

    learned dict

---
+++ dict.py
@@ -0,0 +1,22 @@
+person = {"name": "John","age": 36}
+
+person["country"] = "Nigeria"
+person["score"] = 95.5
+
+person["age"]=36 + 1
+
+print("hello" , person["name"])
+print("you are" , person["age"] ,"years old")
+print("your are from" , person["country"])
+print("your score is" , person["score"])
+print("here is a combination of what i just said:")
+print(person)
+
+print("heres a different arragement;")
+
+print(person["name"])
+print(person["age"])
+print(person["country"])
+print(person["score"])
+
+print("which arrangement suits you better??")
