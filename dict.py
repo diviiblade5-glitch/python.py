c8495c89c77579f4907b894e135ab70ca485d508
diviiblade5-glitch <enenchedivine24@gmail.com>
2026-09-03 14:56:06 +0100

    added next lines

--- dict.py
+++ dict.py
@@ -9,14 +9,15 @@ print("hello" , person["name"])
 print("you are" , person["age"] ,"years old")
 print("your are from" , person["country"])
 print("your score is" , person["score"])
+print("\n")
 print("here is a combination of what i just said:")
 print(person)
-
+print("\n")
 print("heres a different arragement;")
 
 print(person["name"])
 print(person["age"])
 print(person["country"])
 print(person["score"])
-
+print("\n")
 print("which arrangement suits you better??")
