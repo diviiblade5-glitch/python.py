5cf8c96d411a609349d180ae5649ab1ecd324f79
https://github.com/diviiblade5-glitch/python.py.git <diviiblade5@gmail.com>
2026-09-08 09:21:57 +0100

    more practice

---
+++ l.py
@@ -0,0 +1,37 @@
+x = 4
+x ="sally"
+print(x)
+
+print(2*5)
+
+fruits=["apple", "banana", "cherry"]
+x=y=z=fruits
+print(x)
+print(y)
+print(z)
+
+x ="python"
+y ="is"
+z ="awesome"
+print(x+ " "+y +" " + z)
+print(y,x,z)
+
+x =5
+y ="john"
+print(x,y)
+
+FirstName = "JOHN"
+LastName  = "SMITH"
+FullName  = FirstName + " " + LastName
+
+print(FullName)
+
+x=10
+y=3
+
+print(x+y)#adds up value
+print(x-y)#substract value
+print(x * y)#multiplies value
+print(x / y)#divides value and returns with decimal point
+print(x // y)#divides value without returning decimal
+print(x % y)#brings out the remainder of value
